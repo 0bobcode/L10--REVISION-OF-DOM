73a2f4d9946165642599bcef55d841954ff94231
bobchopra <bobchopracoding@gmail.com>
2024-09-10 10:14:06 -0400

    add select varibles and store in elements, created a function called addTask and added validation for task input...

---
+++ script.js
@@ -0,0 +1,10 @@
+//create variables & store elements
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+const addTaskBtn = document.querySelector("#addTaskBtn");
+let taskText = taskInput.value
+console.log(taskText)
+function addTask(){
+    if(taskText !== ""); // == equality operator, ===strictly equality operator, ! not operator
+        taskList.appendChild
+}

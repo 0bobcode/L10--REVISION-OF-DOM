cc51e7f87f3a251875eabd702ada7b263e5cb345
bobchopra <bobchopracoding@gmail.com>
2024-09-11 09:59:27 -0400

    finished to-do-list

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 //create variables & store elements
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
-const addTaskBtn = document.querySelector("#addTaskBtn");
-let taskText = taskInput.value
-console.log(taskText)
-function addTask(){
-    if(taskText !== ""); // == equality operator, ===strictly equality operator, ! not operator
-        taskList.appendChild
+const addTaskBtn = document.getElementById("addTaskBtn");
+
+
+function addTask() {
+
+    let taskText = taskInput.value.trim();
+
+    if(taskText !== "") { 
+        const taskItem = createTaskItem(taskText);
+        taskList.appendChild(taskItem)
+        taskInput.value =""
+    }
+    else {
+        alert("pls enter a task... ")
+    }
 }
+
+function createTaskItem(Task) {
+    const li = document.createElement("li");
+    li.className = "task-item";
+    li.innerHTML = `${Task} <button class="delete-btn">Delete</button>`;
+    let deleteBtn = li.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", () =>li.remove())
+
+    return li;
+}
+
+
+function setupEventListener(){
+    addTaskBtn.addEventListener("click",addTask)
+}
+
+setupEventListener()
